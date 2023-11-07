0564d906e99ba31d2587530519ce10bedd09b39f
mbrambilla <mbrambilla@users.noreply.github.com>
2023-11-07 10:03:48 -0500

    exercises 4-5a

--- src/app/[postSlug]/page.js
+++ src/app/[postSlug]/page.js
@@ -1,6 +1,9 @@
 import React from 'react';
 
+import COMPONENT_MAP from '@/helpers/mdx-components';
+
 import BlogHero from '@/components/BlogHero';
+
 import { BLOG_TITLE } from '@/constants';
 import { loadBlogPost } from '@/helpers/file-helpers';
 import { MDXRemote } from 'next-mdx-remote/rsc';
@@ -8,28 +11,31 @@ import { MDXRemote } from 'next-mdx-remote/rsc';
 import styles from './postSlug.module.css';
 
 export async function generateMetadata({ params }) {
-    const { frontmatter } = await loadBlogPost(params.postSlug);;
+  const { frontmatter } = await loadBlogPost(params.postSlug);;
 
-    return {
-        title: `${frontmatter.title} | ${BLOG_TITLE}`,
-        description: frontmatter.abstract
-    };
+  return {
+    title: `${frontmatter.title} | ${BLOG_TITLE}`,
+    description: frontmatter.abstract
+  };
 }
 
 async function BlogPost({ params }) {
-    const { frontmatter, content } = await loadBlogPost(params.postSlug);;
+  const { frontmatter, content } = await loadBlogPost(params.postSlug);;
 
-    return (
-        <article className={styles.wrapper}>
-            <BlogHero
-                title={frontmatter.title}
-                publishedOn={frontmatter.publishedOn}
-            />
-            <div className={styles.page}>
-                <MDXRemote source={content} />
-            </div>
-        </article>
-    );
+  return (
+    <article className={styles.wrapper}>
+      <BlogHero
+        title={frontmatter.title}
+        publishedOn={frontmatter.publishedOn}
+      />
+      <div className={styles.page}>
+        <MDXRemote
+          source={content}
+          components={COMPONENT_MAP}
+        />
+      </div>
+    </article>
+  );
 }
 
 export default BlogPost;
